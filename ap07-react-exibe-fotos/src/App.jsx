1dc3ba0fb95bf1769ef6b33471fdae858fc709ab
Lia Matsubara <matsubaralia@gmail.com>
2025-11-05 18:06:14 -0300

    learn(ap07-react): encerra a secao 2.12

--- ap07-react-exibe-fotos/src/App.jsx
+++ ap07-react-exibe-fotos/src/App.jsx
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
 import Busca from './components/Busca'
+import { createClient } from 'pexels'
+import PexelsLogo from './components/PexelsLogo'
+import Imagem from './components/Imagem'
 
 export default class App extends Component {
+
+  state = {
+    photos: []
+  }
+
+  pexelsClient = null
+
+  onBuscaRealizada = (termo) => {
+    this.pexelsClient.photos.search({
+      query: termo
+    })
+    .then(result => this.setState({photos: result.photos}))
+  }
+
+  componentDidMount(){
+    this.pexelsClient = createClient('utWGCJucUoEfNKovpH63k01X9KIx1QzozUAFKN1yhxu6efamdpvQJ9J1')
+  }
+
   render() {
     return (
-      <div className='grid w-9 m-auto border-1'>
+      <div className='grid w-9 m-auto border-1 border-400'>
+        <div className="col-12">
+          <PexelsLogo />
+        </div>
         <div className="col-12">
           <h1>Exibir uma lista de...</h1>
         </div>
         <div className="col-12">
-          <Busca dica='procurar'/>
+          <Busca 
+            dica='procurar'
+            onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className="col-12">
+          {
+            this.state.photos.map((photo) => (
+              <div key={photo.id}>
+                <Imagem src={photo.src.small} alt={`Foto tirada por ${photo.photographer}. ${photo.alt}`} />
+              </div>
+            ))
+          }
         </div>
       </div>
     )
