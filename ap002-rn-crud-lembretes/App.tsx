16bc9676d978dca5260becb1642d12c27b751350
Lia Matsubara <matsubaralia@gmail.com>
2025-11-14 11:58:07 -0300

    ap002-rn: lida com a pg 40

--- ap002-rn-crud-lembretes/App.tsx
+++ ap002-rn-crud-lembretes/App.tsx
@@ -1,5 +1,6 @@
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, FlatList, Linking, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useState } from 'react';
+import AntIcon from '@expo/vector-icons/AntDesign';
 
 interface Lembrete{
   id: string;
@@ -11,18 +12,44 @@ export default function App() {
   const [lembretes, setLembretes] = useState<Lembrete[]>([])
 
   const adicionar = () => {
-    const novoLembrete: Lembrete = {
-      id: new Date().getTime().toString(),
-      texto: lembrete
+    if(lembrete.length > 0){
+      const novoLembrete: Lembrete = {
+        id: new Date().getTime().toString(),
+        texto: lembrete.trim()
+      }
+      setLembretes((listaAtual) => ([novoLembrete, ...listaAtual]))
+      setLembrete('')
     }
-    setLembretes((listaAtual) => ([novoLembrete, ...listaAtual]))
-    setLembrete('')
+  }
+
+  const remover = (lembreteARemover: Lembrete) => {
+    // Alert.alert(
+    //   'Remover lembrete',
+    //   `Deseja mesmo remover o seguinte lembrete? ${lembreteARemover.texto}`,
+    //   [
+    //     {
+    //       text: 'Cancelar',
+    //       style: 'cancel'
+    //     },
+    //     {
+    //       text: 'Remover',
+    //       style: 'destructive',
+    //       onPress: () => {
+    //         setLembretes((lembretesAtual: Lembrete[]) => {
+    //           return lembretesAtual.filter((l: Lembrete) => l.id !== lembreteARemover.id)
+    //         })
+    //       }
+    //     }
+    //   ]
+    // )
+
+    setLembretes(lembretesAtual => (lembretesAtual.filter(l => l.id !== lembreteARemover.id)))
   }
 
   return (
     <View style={styles.container}>
       <TextInput
-        value={lembrete.toUpperCase()}
+        value={lembrete}
         onChangeText={setLembrete}
         style={styles.input}
         placeholder='Digite um lembrete...'/>
@@ -31,9 +58,35 @@ export default function App() {
         style={styles.button}>
         <Text style={styles.buttonText}>Salvar lembrete</Text>
       </Pressable>
-      {
-        lembretes.map(l => <Text>{l.texto}</Text>)
-      }
+      <FlatList 
+        style={styles.list}
+        keyExtractor={(l) => l.id}
+        data={lembretes}
+        renderItem={(l) => {
+          return (
+            <View style={styles.listItem}>
+              <Text style={styles.listItemText}>
+                  {l.item.texto}
+              </Text>
+              <View style={styles.listItemButtons}>
+                <Pressable onPress={() => remover(l.item)}>
+                  <AntIcon name='delete' size={24} />
+                </Pressable>
+                <Pressable>
+                  <AntIcon name='edit' size={24} />
+                </Pressable>
+              </View>
+            </View>)
+        }}
+        ItemSeparatorComponent={() => (<View style={{marginVertical: 2}}></View>)}
+        ListEmptyComponent={() => <Text style={{textAlign: 'center'}}>Não temos lembretes ainda.</Text>}
+      />
+      <View style={styles.footer}>
+        <Pressable
+          onPress={() => Linking.openURL('https://github.com/liamatsubara')}>
+          <AntIcon name='github' size={24}/>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -44,6 +97,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
   },
   input: {
     width: '80%',
@@ -63,5 +117,40 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     textAlign: 'center',
+  },
+  list: {
+    width: '80%',
+    borderColor: '#DDD',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginTop: 12,
+    padding: 8
+  },
+  listItem: {
+    padding: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+    borderRadius: 4,
+    backgroundColor: '#F0F0F0',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  listItemText: {
+    textAlign: 'center',
+    width: '70%'
+  },
+  footer: {
+    borderColor: '#DDD',
+    borderWidth: 1,
+    width: '80%',
+    alignItems: 'center',
+    padding: 12,
+    marginTop: 8,
+    borderRadius: 4
+  },
+  listItemButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '30%',
   }
 });
